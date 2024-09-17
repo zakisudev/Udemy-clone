1b8bcb0ed3aa069149861a9d659cee36eee9b434
zakisudev <zakisudev@gmail.com>
2024-09-17 16:02:25 +0300

    Refactor next.config.mjs to add remotePatterns for image uploads Update package.json dependencies for upload functionality and form validation

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { hostname: 'sea1.ingest.uploadthing.com' },
+      { hostname: 'utfs.io' },
+      { hostname: 'img.clerk.com' },
+    ],
+  },
+};
 
 export default nextConfig;
